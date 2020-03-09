274880bf1f0da3961a6fb3f36a2c3f5726e5dadb
S. Makai Takori <makait87@gmail.com>
2020-03-09 17:13:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,13 +32,13 @@ class Dog
 
   def save
  if self.id
-   self.update
+  # self.update
  else
    sql = <<-SQL
-     INSERT INTO songs (name, album)
+     INSERT INTO dogs (name, breed)
      VALUES (?, ?)
    SQL
-   DB[:conn].execute(sql, self.name, self.album)
+   DB[:conn].execute(sql, self.name, self.breed)
    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
  end
 end
