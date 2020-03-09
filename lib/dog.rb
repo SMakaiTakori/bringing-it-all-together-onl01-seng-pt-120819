5d780abed42ed3ac16e1e0c19961f74fcbf548ba
S. Makai Takori <makait87@gmail.com>
2020-03-09 17:47:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,7 +67,7 @@ class Dog
       SELECT * FROM dogs WHERE name = ?
     SQL
     
-    DB[:conn].execute(sql, name) do |row|
+    DB[:conn].execute(sql, name).collect do |row|
       self.new_from_db(row)
     end.first
       
